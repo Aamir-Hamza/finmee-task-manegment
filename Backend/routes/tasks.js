ac57c7c1bb0da2da77ee4de486e235e630091d6c
Aamiramza <ek939774@gmail.com>
2025-06-26 16:10:01 +0530

    commit

--- Backend/routes/tasks.js
+++ Backend/routes/tasks.js
@@ -27,7 +27,7 @@ router.post('/', auth, async (req, res) => {
       user: req.user.id,
     });
     await task.save();
-    res.status(201).json(task);
+    res.status(201).json({ task });
   } catch (err) {
     res.status(500).json({ message: 'Server error.' });
   }
